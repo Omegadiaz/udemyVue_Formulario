7f9fd4b136c9aa2ecdf5395766f4587dcf41e08a
Omegadiaz <omsystem.gabriel@gmail.com>
2021-07-31 05:05:30 +0200

    Formulario de registro

---
+++ src/views/Registro.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1>Registrar nuevo usuario:</h1>
+  <form class="form-group">
+      <input 
+      class="form-control  my-2" 
+      placeholder="email" 
+      type="email"
+      v-model.trim="email"/>
+
+      <input class="form-control  my-2" 
+      placeholder="password" 
+      type="password"
+      v-model.trim="password1"/>
+
+      <input class="form-control my-3" 
+      placeholder="password" 
+      type="password"
+      v-model.trim="password2"/>
+
+      <button 
+      type="submit"
+      class="btn btn-primary"
+      :disabled="bloquear"
+        >Confirmar</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      email: '',
+      password1: '',
+      password2: ''
+    }
+  },
+  computed: {
+    bloquear(){
+      if(!this.email.includes('@')){
+        return true
+      }
+      if(this.password1 === this.password2 && this.password1.length > 5){
+        return false
+      }      
+      return true
+    }
+  }
+}
+</script>
